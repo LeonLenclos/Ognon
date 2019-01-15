9157ddc8ecc542a65864476d4e8ab72e0a99c403
Léon Lenclos <leon.lenclos@gmail.com>
2019-01-15 21:21:31 +0100

    clean

--- ognon/model/touch.py
+++
@@ -1,8 +0,0 @@
-def touch(fun):
-    """décorateur des methodes du model"""
-    def wrapper(*args):
-        ret = fun(*args)
-        for l in args[0].listeners:
-            l()
-        return ret
-    return wrapper
