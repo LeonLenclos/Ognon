eda1a5a5ab6d46976f7c30855cbdc8d2c5867cbd
Léon Lenclos <leon.lenclos@gmail.com>
2019-02-06 11:21:24 +0100

    update __main__.py with args parsing

--- ognon/__main__.py
+++ ognon/__main__.py
@@ -1,17 +1,37 @@
 """
-This module prints information about the Ognon session and start the server.
+This module start the server.
 """
 
 import pytest
+import argparse
+import webbrowser
 
 from . import server
 from . import __version__, PROJECTS_DIR, DEFAULT_ADRESS
 
-pytest.main(['-x', 'ognon/tests'])
 
-title = ' Ognon v{} '.format(__version__)
-print('+{line}+\n|{title}|\n+{line}+'.format(line='-'*len(title), title=title))
-print("Working on file://{}".format(PROJECTS_DIR))
-print("Serving on http://{}:{}".format(*DEFAULT_ADRESS))
+parser = argparse.ArgumentParser(
+	prog='python -m ognon',
+	description='Start the Ognon server.')
+parser.add_argument('-t', '--test',
+	action='store_true',
+	help='run tests and exit')
+parser.add_argument('-b', '--browse',
+	action='store_true',
+	help='open the adress in a web browser')
 
-server.serve(DEFAULT_ADRESS)
+def main(args):
+	if args.test:
+		return pytest.main(['-x', 'ognon/tests'])
+
+	title = ' Ognon v{} '.format(__version__)
+	print('+{line}+\n|{title}|\n+{line}+'.format(line='-'*len(title), title=title))
+	print("Working on file://{}".format(PROJECTS_DIR))
+	print("Serving on http://{}:{}".format(*DEFAULT_ADRESS))
+
+	if args.browse:
+		webbrowser.open_new("http://{}:{}".format(*DEFAULT_ADRESS))
+
+	server.serve(DEFAULT_ADRESS)
+
+main(parser.parse_args())
