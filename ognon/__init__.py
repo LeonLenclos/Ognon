f37561dd41ec35d3edf8244aaf46e9de60fdadc9
Léon Lenclos <leon.lenclos@gmail.com>
2019-02-27 19:22:47 +0100

    fix typo in docstring

--- ognon/__init__.py
+++ ognon/__init__.py
@@ -1,6 +1,6 @@
 """
 Ognon is a a local-only web application for the creation of 2D animation.
-Run `python -m ognon` to start the server. Then browse http://lacalhost:40460
+Run `python -m ognon` to start the server. Then browse http://localhost:40460
 """
 import os
 
@@ -8,8 +8,6 @@ __version__ = '1.α'
 HTTP_ADRESS = ('localhost', 40460)
 OSC_ADRESS = ('localhost', 50460)
 PROJECTS_DIR = os.path.expanduser('~/ognons/')
-
-print(os.path.join(os.path.dirname(__file__), 'tests'))
 
 from . import model
 from . import server
