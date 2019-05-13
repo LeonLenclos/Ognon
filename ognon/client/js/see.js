e7ee1f8fbaa92a5276a5bed47f5a4daecb6fe7f3
Léon Lenclos <leon.lenclos@gmail.com>
2019-05-14 00:33:07 +0200

    update see page (from edit.html)

--- ognon/client/js/see.js
+++ ognon/client/js/see.js
@@ -3,5 +3,21 @@ modules = [canvas];
 
 
 ///////////// SETUP
-callModulesMethod('setup');
-startAutoUpdate();
+
+fetch('/view/get_project_defined/', initOptions())
+.then(handleResponse)
+.then(project_defined=>{
+    if (!project_defined){
+        fetch('/control/projectmanager/get', initOptions({name:'my_project'}))
+        .then(handleResponse)
+        .then(()=>{
+            callModulesMethod('setup');
+            startAutoUpdate();
+        })
+        .catch(handleError);
+    } else {
+        callModulesMethod('setup');
+        startAutoUpdate();
+    }
+})
+.catch(handleError);
