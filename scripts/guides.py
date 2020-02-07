e911e055a7b134e779f304336864eeff672e858d
Léon Lenclos <leon.lenclos@gmail.com>
2020-02-07 16:43:37 +0100

    script

--- scripts/guides.py
+++ scripts/guides.py
@@ -5,12 +5,12 @@ Create a .ogn file with guides.
 from ognon import projects
 from ognon.model import Cell, Line, Anim, Layer
 
-project_name = 'nata'
-guide_name = 'hitbox'
-width, height = 400, 600
+project_name = 'to'
+guide_name = 'cache'
+width, height = 2048, 1080
 hlines = [] # list of horizontal lines y positions
-vlines = [] # list of vertical lines x positions
-rectangles = [(50,50,width-50,height-50)] # list of rectangles (x1,y1,x2,y2) position
+vlines = [25, width-25] # list of vertical lines x positions
+rectangles = [] # list of rectangles (x1,y1,x2,y2) position
 
 
 cell = Cell(lines=[
