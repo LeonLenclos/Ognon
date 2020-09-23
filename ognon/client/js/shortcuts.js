1e939dccbfa3bedd6cad6abcb983d153595d4975
Léon Lenclos <leon.lenclos@gmail.com>
2020-09-23 23:03:39 +0200

    turing test shortcuts

--- ognon/client/js/shortcuts.js
+++ ognon/client/js/shortcuts.js
@@ -17,4 +17,18 @@ let ognKeyboardShortcuts = {
     's':{action:'navigationNextFrm'},
     'a':{action:'navigationAutoRun'},
     'q':{action:'navigationPlay'},
-}
+};
+
+// Add Turing Test Shortcucts.
+let ttKeyboardShorcuts = {
+    '=':{action:'projectGet', args:{name:'tt1'}},
+    '$':{action:'projectGet', args:{name:'tt2'}},
+    '*':{action:'projectGet', args:{name:'tt3'}},
+    'œ':{action:'animSelect', args:{name:'master'}},
+    '&':{action:'animSelect', args:{name:'a'}},
+    'é':{action:'animSelect', args:{name:'b'}},
+    '"':{action:'animSelect', args:{name:'c'}},
+    '\'':{action:'animSelect', args:{name:'d'}},
+    '(':{action:'animSelect', args:{name:'e'}},
+};
+Object.assign(ognKeyboardShortcuts, ttKeyboardShorcuts)
